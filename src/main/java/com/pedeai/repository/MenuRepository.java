e5e9c20849db8abe0aa03e3a2b26956fd34620c8
victor <victoor2k19@gmail.com>
2026-02-16 17:53:28 -0300

    creating respositories

---
+++ src/main/java/com/pedeai/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package com.pedeai.repository;
+
+import com.pedeai.entity.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
